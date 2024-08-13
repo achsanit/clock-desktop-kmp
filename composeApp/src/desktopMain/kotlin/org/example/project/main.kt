241a9f00c0e1a0537904b12264c8906a6a6a6a92
Achsani Taqwim <achsani.taqwim@pactindo.com>
2024-08-13 15:42:13 +0700

    refactor(main.kt): remove unnecessary import

--- composeApp/src/desktopMain/kotlin/org/example/project/main.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/main.kt
@@ -1,25 +1,13 @@
 package org.example.project
 
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import java.awt.GraphicsEnvironment
 
 fun main() = application {
-    val screenSize = remember {
-        GraphicsEnvironment
-            .getLocalGraphicsEnvironment()
-            .defaultScreenDevice
-            .displayMode
-    }
     Window(
         onCloseRequest = ::exitApplication,
         state = rememberWindowState(
